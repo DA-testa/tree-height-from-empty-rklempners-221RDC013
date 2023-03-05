ea6311f8c161acd670d9ac3822af867aa56ba707
rklempners-221RDC013 <ralfs.klempners@edu.rtu.lv>
2023-03-05 23:22:42 +0200

    1st commit

--- tree_height.py
+++ tree_height.py
@@ -7,8 +7,26 @@ import numpy
 
 def compute_height(n, parents):
     # Write this function
+    height = {}
+
+    def what_is_the_height(node):
+        if node in height:
+            return height[node]
+        
+        if parents[node] == -1:
+            height[node] = 1
+
+        else:
+            height[node] = 1 + what_is_the_height(parents[node])    
+
+        return height[node]
+    
     max_height = 0
+    
     # Your code here
+    for i in range(n):
+        max_height = max(max_height, what_is_the_height(i))
+    
     return max_height
 
 
@@ -19,8 +37,13 @@ def main():
     # account for github input inprecision
     
     # input number of elements
+    n = int(input())
+
     # input values in one variable, separate with space, split these values in an array
+    parents = list(map(int, input().split()))
+
     # call the function and output it's result
+    print(compute_height(n,parents))
     pass
 
 # In Python, the default limit on recursion depth is rather low,
